81dec136e2e52a4d7de66640dbd19def97844ee7
AMM-JA <emoja.loveyour@gmail.com>
2023-02-26 10:28:59 +0630

    FreeCodeCamp Index Class and function

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,32 @@
 "use strict";
-console.log('testing live server');
+class UserIndex {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "Jaipur";
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class SubUser extends UserIndex {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const hiteshIndex = new UserIndex("h@h.com", "hitesh");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,53 @@
 // console.log(age)
 
 // ! FreeCodeCamp
-console.log('testing live server')
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+class UserIndex {
+
+    protected _courseCount = 1 //? protected mean can't use form public but can use in class inheritance
+    readonly city: string = "Jaipur"
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string
+        ){
+    }
+    // private deleteToken(){ //? private can use within class
+    //     console.log("Token deleted");
+    // }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+}
+
+class SubUser extends UserIndex {
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
+
+const hiteshIndex = new UserIndex("h@h.com","hitesh")
+// hitesh.name 
+
+// hiteshIndex.deleteToken()
